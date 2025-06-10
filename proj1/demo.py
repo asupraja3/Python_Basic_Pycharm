4c237a2bb7822c3c03d086f59ef4a474a3de9a78
Supraja Aravamudhan <asupraja527@gmail.com>
2025-06-09 21:28:37 -0500

    second
    second

--- proj1/demo.py
+++ proj1/demo.py
@@ -1,5 +1,18 @@
 from Calc import *
-c = add(3,4)
-print(c)
+class Computer:
+    def config(self):
+        print("i5,16GB")
 
-print(__name__)
+a = '8'
+print(type(a))
+#str is an inbuilt class: see the output
+#As everything is an object in Python
+comp1 = Computer()
+print(type(comp1))
+
+#1.one way of calling a method
+comp1.config()
+#2.second way of calling method
+Computer.config(comp1)
+a=5
+a.bit_length()
